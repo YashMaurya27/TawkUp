0847308a2be959874651917cdf67e26839cbd940
YashMaurya27 <yash36maurya@gmail.com>
2024-02-27 12:01:15 +0530

    Server deployed & frontend URL changed accordingly

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -29,10 +29,10 @@ export default function Home() {
     process.env.NODE_ENV === "development"
       ? "http://localhost:3000"
       : "https://tawkup.netlify.app/";
-  const socket = io.connect("http://localhost:5000", {
-    transports: ["websocket"],
-  });
-  // const socket = io.connect("https://tawkup.netlify.app/.netlify/functions/server", { transports: ["websocket"] });
+  // const socket = io.connect("http://localhost:5000", {
+  //   transports: ["websocket"],
+  // });
+  const socket = io.connect("https://tawkup-2.onrender.com", { transports: ["websocket"] });
   const fetchCurrentUser = async () => {
     const userNode = await fetchNodeIDbyUserId(userID, usersRef);
     if (userNode && userNode !== null) {
